c79aae9cd7756653d6f08c6eb7b904ecfdd753a9
Blake Danson <blakedanson@gmail.com>
2022-03-28 17:39:51 -0700

    Update fn name to get_sentiment_predictions

--- app/api/text.py
+++ app/api/text.py
@@ -1,7 +1,7 @@
 import os
 import requests
 from fastapi import APIRouter
-from app.services.text import get_text_embeddings
+from app.services.text import get_sentiment_predictions
 
 router = APIRouter()
 
@@ -9,4 +9,4 @@ router = APIRouter()
 @router.get("/text/", tags=["text"])
 async def read_text():
     inputs = ["so stupid, anger, rage", "it was ok I guess", "happy happy joy joy"]
-    return {"inputs": inputs, "results": get_text_embeddings(inputs)}
+    return {"inputs": inputs, "results": get_sentiment_predictions(inputs)}
--- app/services/text.py
+++ app/services/text.py
@@ -4,7 +4,7 @@ import requests
 MODEL_BASE_URL = os.environ.get("MODEL_BASE_URL", "localhost:8501")
 
 
-def get_text_embeddings(texts: list):
+def get_sentiment_predictions(texts: list):
     data = {"inputs": texts}
     resp = requests.post(f"http://{MODEL_BASE_URL}/v1/models/sentiment_model:predict", json=data).json()
     embeddings = resp["outputs"]
