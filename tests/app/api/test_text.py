d039298a6c6ad60577d006b4cfd4268e036594b0
Blake Danson <blakedanson@gmail.com>
2022-04-09 11:40:58 -0700

    Add test for sentiment predictions

--- tests/app/api/test_text.py
+++ tests/app/api/test_text.py
@@ -1,7 +1,10 @@
+import json
 from fastapi import FastAPI
 from fastapi.testclient import TestClient
 from unittest import mock
 from app.main import app
+from app.models.text import TextRequest
+
 
 client = TestClient(app)
 
@@ -17,4 +20,21 @@ def test_get_example():
                 "It was the worst movie I have ever seen. Huge waste of time",
             ],
             "results": [1, 0.5, 0.0001],
+        }
+
+
+def test_predict_sentiment():
+    with mock.patch("requests.post") as mock_requests:
+        mock_requests.return_value.json.return_value = {"outputs": [0.5]}
+        resp = client.post(
+            "/text/sentiment/predict",
+            json=TextRequest(
+                texts=["I'm feeling sentimental, please tell me approximately how sentimental."]
+            ).dict(),
+        )
+        assert resp.json() == {
+            "inputs": [
+                "I'm feeling sentimental, please tell me approximately how sentimental.",
+            ],
+            "results": [0.5],
         }
